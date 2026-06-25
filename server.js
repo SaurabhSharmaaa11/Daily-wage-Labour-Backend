a5a05e4bc0216c771f81092f627b992f1d3568d5
Saurabh Sharma <saurabhsharmaaa11>
2026-06-25 15:40:39 +0530

    fix workerRoutes import path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const WorkerRoutes = require("./routes/WorkerRoutes");
+ const WorkerRoutes = require("./routes/workerRoutes");
 
 
 const app = express();
