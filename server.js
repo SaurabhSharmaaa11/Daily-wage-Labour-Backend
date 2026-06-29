4890521324a892acc9a59489ef2b7721adad8551
Saurabh Sharma <saurabhsharmaaa11>
2026-06-29 16:48:00 +0530

    fix mongodb connection

--- server.js
+++ server.js
@@ -2,46 +2,48 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");       
+const mongoose = require("mongoose");
 
 const WorkerRoutes = require("./routes/workerRoutes");
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
 
-// Test route
+app.use("/api/Worker", WorkerRoutes);
+
+
 app.get("/", (req,res)=>{
     res.send("Backend Running Successfully");
 });
 
 
-// Routes
-app.use("/api/Worker", WorkerRoutes);
-
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-
-    console.log("MongoDB Connected Successfully");
-
-})
-.catch((error)=>{
-
-    console.log("MongoDB Error:");
-    console.log(error);
-
-});
-
-
-// IMPORTANT FOR RENDER
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
 
-    console.log(`Server running on port ${PORT}`);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected Successfully");
 
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Server running on port", process.env.PORT || 5000);
+    });
+
+})
+.catch((error) => {
+    console.log("MongoDB Error:");
+    console.log(error);
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected Successfully");
+    console.log("Database Ready ✅");
+})
+.catch((error) => {
+    console.log("MongoDB Error:");
+    console.log(error);
 });
